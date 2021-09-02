d50e0f2ab6bb9e9976ecff1598ed9225f7d91cb7
joaopjk <joaopjk48@gmail.com>
2021-09-01 22:35:43 -0300

    Create models and ef database context for the products class

--- GrpcMicroservice/ProductGrpc/Startup.cs
+++ GrpcMicroservice/ProductGrpc/Startup.cs
@@ -1,12 +1,10 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ProductGrpc.Data;
 
 namespace ProductGrpc
 {
@@ -17,6 +15,10 @@ namespace ProductGrpc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpc();
+            services.AddDbContext<ProductsContext>(options =>
+            {
+                options.UseInMemoryDatabase("Produtcs");
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
