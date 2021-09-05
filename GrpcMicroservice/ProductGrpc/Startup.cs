8318a42e639c6e13caed9911e86dbb343c636f21
joaopjk <joaopjk48@gmail.com>
2021-09-05 18:00:23 -0300

    Developing product.proto file for exposing CRUD services in ProductsGrpcAPI

--- GrpcMicroservice/ProductGrpc/Startup.cs
+++ GrpcMicroservice/ProductGrpc/Startup.cs
@@ -33,8 +33,6 @@ namespace ProductGrpc
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGrpcService<GreeterService>();
-
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
