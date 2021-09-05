0ebb60c332b37c293b50b9b3479e2869e27f1b60
joaopjk <joaopjk48@gmail.com>
2021-09-05 17:41:11 -0300

    Seeding in memory database with EF for ProducGRPC service

--- GrpcMicroservice/ProductGrpc/Program.cs
+++ GrpcMicroservice/ProductGrpc/Program.cs
@@ -22,9 +22,7 @@ namespace ProductGrpc
             ProductsContextSeed.SeedAsync(productsContext);
         }
 
-        // Additional configuration is required to successfully run gRPC on macOS.
-        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+       public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
