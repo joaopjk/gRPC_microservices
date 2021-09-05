8a4fa50a54dd9b8f06234421019e2000d6eb51c9
joaopjk <joaopjk48@gmail.com>
2021-09-05 18:30:13 -0300

    Create client console application for consuming ProductGrpcMicroservice

---
+++ GrpcMicroservice/ProductGrpcClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductGrpcClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
