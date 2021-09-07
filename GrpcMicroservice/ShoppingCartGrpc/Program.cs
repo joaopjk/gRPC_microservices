2ad964a4c8a982af685e214c1d9ce381d0bdf480
joaopjk <joaopjk48@gmail.com>
2021-09-07 15:45:27 -0300

    Seed in-memory database ShoppingCart

--- GrpcMicroservice/ShoppingCartGrpc/Program.cs
+++ GrpcMicroservice/ShoppingCartGrpc/Program.cs
@@ -1,10 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using ShoppingCartGrpc.Data;
 
 namespace ShoppingCartGrpc
 {
@@ -12,11 +9,17 @@ namespace ShoppingCartGrpc
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            SeedDatabase(host);
+            host.Run();
         }
-
-        // Additional configuration is required to successfully run gRPC on macOS.
-        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+        private static void SeedDatabase(IHost host)
+        {
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var productsContext = services.GetRequiredService<ShoppingCartContext>();
+            ShoppingCartContextSeed.SeedAsync(productsContext);
+        }
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
