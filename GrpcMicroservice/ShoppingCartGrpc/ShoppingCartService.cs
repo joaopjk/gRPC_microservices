4cb793aa7834e3d9c425ec935bf092bca3c6f0c7
joaopjk <joaopjk48@gmail.com>
2021-09-07 15:57:59 -0300

    GetShoppingCart

---
+++ GrpcMicroservice/ShoppingCartGrpc/ShoppingCartService.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ShoppingCartGrpc.Data;
+using ShoppingCartGrpc.Protos;
+
+namespace ShoppingCartGrpc
+{
+    public class ShoppingCartService : ShoppingCartProtoService.ShoppingCartProtoServiceBase
+    {
+        private readonly ShoppingCartContext _shoppingCartDbContext;
+                private readonly ILogger<ShoppingCartService> _logger;
+        public ShoppingCartService(ShoppingCartContext shoppingCartDbContext, ILogger<ShoppingCartService> logger)
+        {
+            _shoppingCartDbContext = shoppingCartDbContext;
+            _logger = logger;
+        }
+
+        public override async Task<ShoppingCartModel> GetShoppingCart(GetShoppingCartRequest request, ServerCallContext context)
+        {
+            var shoppingCart =
+                await _shoppingCartDbContext.ShoppingCart.FirstOrDefaultAsync(s => s.UserName == request.Username);
+
+            if (shoppingCart == null)
+                throw new RpcException(new Status(StatusCode.NotFound,
+                    $"Shopping Cart whit UserName={request.Username} not found"));
+
+            //var shoppingCartModel = _mapper.Map<ShoppingCartModel>(shoppingCartModel)
+            var shoppingCartModel = new ShoppingCartModel();
+            return shoppingCartModel;
+        }
+
+        public override Task<ShoppingCartModel> CreateShoppingCart(ShoppingCartModel request, ServerCallContext context)
+        {
+            return base.CreateShoppingCart(request, context);
+        }
+
+        public override Task<AddItemIntoShoppingCartResponse> AddItemIntoShoppingCart(IAsyncStreamReader<AddItemIntoShoppingCartRequest> requestStream, ServerCallContext context)
+        {
+            return base.AddItemIntoShoppingCart(requestStream, context);
+        }
+
+        public override Task<RemoveItemIntoShoppingCartResponse> RemoveItemIntoShoppingCart(RemoveItemIntoShoppingCartRequest request, ServerCallContext context)
+        {
+            return base.RemoveItemIntoShoppingCart(request, context);
+        }
+    }
+}
