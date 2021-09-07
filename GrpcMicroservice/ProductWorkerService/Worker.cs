b8c2e7896229eb11b5a06c7df8236f355c204ef5
joaopjk <joaopjk48@gmail.com>
2021-09-06 22:29:11 -0300

    WokerService add products

--- GrpcMicroservice/ProductWorkerService/Worker.cs
+++ GrpcMicroservice/ProductWorkerService/Worker.cs
@@ -1,9 +1,13 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
 using Microsoft.Extensions.Configuration;
+using ProductGrpc.Protos;
 
 namespace ProductWorkerService
 {
@@ -19,9 +23,32 @@ namespace ProductWorkerService
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            Thread.Sleep(5000);
+
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+
+                var httpHandler = new HttpClientHandler();
+                httpHandler.ServerCertificateCustomValidationCallback =
+                    HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+                using var channel = GrpcChannel.ForAddress(_configuration["WorkerService:ServerUrl"], new GrpcChannelOptions { HttpHandler = httpHandler });
+                var client = new ProductProtoService.ProductProtoServiceClient(channel);
+
+                var addProductAsync = await client.AddProductAsync(new AddProductRequest()
+                {
+                    Product = new ProductModel()
+                    {
+                        Name = _configuration["WorkerService:ProductName"] + DateTime.Now,
+                        Description = _configuration["WorkerService:ProductName"],
+                        Price = 1000,
+                        Status = ProductStatus.Instock,
+                        CreatedTime = Timestamp.FromDateTime(DateTime.UtcNow)
+                    }
+                });
+                Console.WriteLine("AddProductAsync" + addProductAsync.ToString());
+
                 await Task.Delay(Convert.ToInt32(_configuration["WorkerService:TaskInterval"]), stoppingToken);
             }
         }
